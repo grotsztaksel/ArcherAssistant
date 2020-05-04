fd17d506538939085b7bfe83996bf4f14d86a35b
grotsztaksel <grotsztaksel@o2.pl>
2020-05-04 23:04:19 +0200

    DataManager: Implement more functions

--- src/data_manager/datamanager.cpp
+++ src/data_manager/datamanager.cpp
@@ -4,22 +4,29 @@
 #include "pugixml.hpp"
 
 
-
+using namespace pugi;
 DataManager::DataManager(QObject *parent, const QStringList args, const bool gui) : QObject(parent), args{args}, gui{gui}
 // used gui{gui} in member initializer lists, even though it is still in the args - because gui is const
 {
     settings = new QSettings(QSettings::UserScope,"Home", "ArcherAssistant",this);
-
-    // check if the args contain the config file
-    setupConfigFile();
+    setUp();
+    openConfigFile();
 }
 
 DataManager::~DataManager()
 {
-    if (cfg.opened) {
+    if (cfg.opened && cfg.modified) {
         cfg.pugi.save_file(cfg.file->fileName().toStdString().c_str(), "  ");
+        qDebug() << "Modified file"<<cfg.file->fileName();
     }
 }
+
+void DataManager::setUp()
+{
+    setupConfigFile();
+    setupIntervals();
+}
+
 
 void DataManager::setupConfigFile()
 {
@@ -32,10 +39,22 @@ void DataManager::setupConfigFile()
             }
         }
     }
-
     if (!settings->contains("configFile")){
+        qDebug()<<"setting configFile";
         settings->setValue("configFile", QString("config.xml"));
     }
+}
+
+void DataManager::setupIntervals(){
+    if (!settings->contains("sessionInterval")){
+        qint64 sessionInterval = 60 * 20; // 60 sec times minutes
+        settings->setValue("sessionInterval", sessionInterval);
+    }
+    if (!settings->contains("seriesInterval")){
+        qint64 seriesInterval = 60 * 4; // 60 sec times minutes
+        settings->setValue("seriesInterval", seriesInterval);
+    }
+
 }
 
 
@@ -60,13 +79,57 @@ bool DataManager::openConfigFile(){
     }
     cfg.file = new QFile(settings->value("configFile").toString(), this);
     cfg.opened = cfg.pugi.load_file(cfg.file->fileName().toStdString().c_str());
+    if (cfg.opened) cfg.setBasicStructure();
     return cfg.opened;
 }
 
+
 void DataManager::updateSessions(){
     auto imageDir = this->getImagesPath();
-    QFileInfoList fileInfos = imageDir.entryInfoList(settings->value("imagesFilter").toStringList(),QDir::Files);
-    for (QFileInfo fileInfo : fileInfos){
+    // get the list of inage files, sorted by creation time
+    QFileInfoList fileInfos = imageDir.entryInfoList(settings->value("imagesFilter").toStringList(),QDir::Files, QDir::Time);
+    qint64 sessionInterval = getSessionInterval();
+    qint64 seriesInterval = getSessionInterval();
 
+
+    // go to the last save session/series to get the date
+    xml_node sessionsNode = cfg.pugi.document_element().child("sessions");
+    if (sessionsNode.empty()){}
+//    xml_node lastSeries = cfg.pugi.document_element().child("sessions").last_child("session").last_child("series");
+//    if (lastSeries.empty()){
+//        //create the
+//    }
+//    for (QFileInfo fileInfo : fileInfos){
+//    }
+
+}
+qint64 DataManager::getSessionInterval()
+{
+    return settings->value("sessionInterval").toInt();
+}
+
+qint64 DataManager::getSeriesInterval()
+{
+    return settings->value("seriesInterval").toInt();
+}
+
+void config::setBasicStructure()
+{
+    root = pugi.document_element();
+    if (QString(root.name()) != "ArcherAssistant"){
+        root.set_name("ArcherAssistant");
+        qDebug() << "Setting name";
+        setModified();
     }
+    xml_node sessions = root.child("sessions");
+    if (sessions == nullptr) {
+        root.append_child("sessions");
+        qDebug() <<"Adding sessions";
+        setModified();
+    }
+}
+
+void config::setModified()
+{
+    modified = true;
 }
--- src/data_manager/datamanager.h
+++ src/data_manager/datamanager.h
@@ -6,12 +6,22 @@
 #include <QString>
 #include <QFile>
 #include <QDir>
+#include <QDateTime>
+#include <QTime>
 
 #include <pugixml.hpp>
+
 struct config{
     QFile *file = nullptr;
     pugi::xml_document pugi;
+    pugi::xml_node root;
     bool opened = false;
+    bool modified = true;
+    //! Create the basic structure of the config tree. If the structure,
+    //! or its parts are already present, keep them
+    void setBasicStructure();
+    void setModified();
+
 };
 
 class DataManager : public QObject
@@ -27,9 +37,16 @@ public slots:
     //! open the config xml file
     bool openConfigFile();
 private: //methods
+
+    //! create default settings, if they are absent
+    void setUp();
+
     //! take the input argument xml="<filename>" and save it into the program settings.
     //! If the argument is not present, use the default one
     void setupConfigFile();
+
+    //! set up default time intervals
+    void setupIntervals();
 
     //! read the config xml data and find the information about the location of the images on the drive system
     QDir getImagesPath();
@@ -37,6 +54,17 @@ private: //methods
     //! search the paths provided and find all the image files. Check the date of creation, and create a <series> node
     //! in <session> with date and times attributes.
     void updateSessions();
+
+    //! get the interval between sessions (in seconds). If the two images were created within interval shorter than this one (but longer than
+    //! \ref seriesInterval seriesInterval()), they are considered to belong to the same session. Otherwise, finding an image created later triggers
+    //! creation of a new session node
+    qint64 getSessionInterval();
+
+    //! \anchor seriesInterval get the difference between shooting series (in seconds). If the two images were created within interval shorter than this one,
+    //! they are considered to document the same series (e.g photos taken from different angles, or lighting conditions to
+    //! catch the locations of arrows better). Otherwise, finding an image created later triggers
+    //! creation of a new series node
+    qint64 getSeriesInterval();
 
 
 private: // variables
@@ -48,8 +76,6 @@ private: // variables
     QSettings *settings = nullptr;
     //! the actual data structure collecting the QFile object and the pugi_xml object to work together
     config cfg;
-
-
 
 signals:
 
