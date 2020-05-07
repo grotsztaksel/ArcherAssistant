826ecc6c9aad984984a1d4217df95a7b95830736
grotsztaksel <grotsztaksel@o2.pl>
2020-05-07 23:08:59 +0200

    DataManager: Implement first methods in DataManager

--- src/data_manager/datamanager.cpp
+++ src/data_manager/datamanager.cpp
@@ -1,7 +1,7 @@
 #include "datamanager.h"
 
 #include <QDebug>
-
+#include <QFileInfo>
 using namespace pugi;
 DataManager::DataManager(QObject* parent) : QObject(parent) {}
 
@@ -22,11 +22,104 @@ void DataManager::setBasicStructure() {
 
 DataManager::~DataManager() {
   if (opened && modified) {
-    pugi.save_file(file->fileName().toStdString().c_str(), "  ");
-    qDebug() << "Modified file" << file->fileName();
+    pugi.save_file(m_file->fileName().toStdString().c_str(), "  ");
+    qDebug() << "Modified file" << m_file->fileName();
   }
 }
 
 void DataManager::setModified() {
   modified = true;
+}
+
+bool DataManager::isModified() const {
+  return modified;
+}
+
+QFile* DataManager::file() const {
+  return m_file;
+}
+
+QList<AATreeNode_abstract*> DataManager::getSessions() {
+  QList<AATreeNode_abstract*> output;
+  xml_node sessionsNode = root.child("sessions");
+  for (xml_node session : sessionsNode.children("session")) {
+  }
+}
+
+bool DataManager::openConfigFile(const QString& fileName) {
+  if (m_file != nullptr) {
+    m_file->deleteLater();
+  }
+  m_file = new QFile(fileName, this);
+  opened = pugi.load_file(m_file->fileName().toStdString().c_str());
+  if (opened)
+    setBasicStructure();
+  return opened;
+}
+
+QList<QDir> DataManager::getImagesPaths() {
+  // Get the m_file's location (because paths may be relative)
+  QFileInfo info = QFileInfo(*m_file);
+  QDir xmlfileDir = info.path();
+  QStringList relativePaths;
+  QList<QDir> output;
+  qDebug() << "Naa?";
+  xml_node imagePathsNode = root.child("imagePaths");
+  for (xml_node path : imagePathsNode.children("path")) {
+    qDebug() << "OI";
+    QString pathValue = QString(path.attribute("dir").value());
+    QString absPath = xmlfileDir.absoluteFilePath(pathValue);
+    QDir absDir = QDir(absPath);
+    if (absDir.exists()) {
+      output << absDir;
+    } else {
+      QDir absPath = QDir(pathValue);
+      if (absPath.isAbsolute()) {
+        output << absPath;
+      }
+    }
+  }
+  auto stuff = pugi.select_nodes("ArcherAssistant/sessions/session/series/hit");
+  for (auto s : stuff) {
+    qDebug() << "beep";
+    qDebug() << QString::fromStdString(s.node().path());
+  }
+  return output;
+}
+
+void DataManager::getLastResult(QDateTime notYoungerThan) {
+  QDateTime latest = QDateTime::fromString("1/01/1900", "d/MM/YYYY");
+  if (!notYoungerThan.isValid()) {
+    notYoungerThan = QDateTime::currentDateTime();
+  }
+  xml_node latestNode;
+  auto sessions = pugi.select_nodes("/ArcherAssistant/sessions");
+
+  for (auto session : sessions) {
+    auto attribute = session.node().attribute("Date");
+    if (!attribute)
+      continue;
+    QDate date =
+        QDate::fromString(QString(session.node().attribute("Date").value()));
+    QTime time =
+        QTime::fromString(QString(session.node().attribute("Time").value()));
+    QDateTime dt = QDateTime(date, time);
+    if (!latest.isValid() || dt > latest) {
+      latest = dt;
+      latestNode = session.node();
+    }
+  }
+  auto seriesSet = latestNode.children("series");
+  for (auto series : seriesSet) {
+    auto attribute = series.attribute("Date");
+    if (!attribute)
+      continue;
+    QDate date = QDate::fromString(QString(series.attribute("Date").value()));
+    QTime time = QTime::fromString(QString(series.attribute("Time").value()));
+    QDateTime dt = QDateTime(date, time);
+    if (dt > latest && dt <= notYoungerThan) {
+      latest = dt;
+      latestNode = series;
+    }
+  }
 }
--- src/data_manager/datamanager.h
+++ src/data_manager/datamanager.h
@@ -12,7 +12,7 @@
 #include <QTime>
 
 #include <pugixml.hpp>
-
+#include "aatreenode_abstract.h"
 //! DataManager does the following:
 //! - Keep the access to the config XML file
 //! - Keep the internal XML structure based on an XML library
@@ -25,33 +25,61 @@ class DataManager : public QObject {
   //! When save the file if it has been modified
   ~DataManager();
 
-  //! Pointer to the QFile object
-  QFile* file = nullptr;
-  //! The actual XML data structure
-  pugi::xml_document pugi;
-  //! Root node, for more convenient navigation
-
-  //! Flag informing if the file has been opened
-  bool opened = false;
-  //! Flag informing, if the file has been modified
-  bool modified = false;
-
   //! Create the basic structure of the config tree. If the structure,
   //! or its parts are already present, keep them
   void setBasicStructure();
 
+  //! Get QFile object associated with the xml data file
+  QFile* file() const;
+
+  //! Return list of pointers to the Session objects
+  QList<AATreeNode_abstract*> getSessions();
+  //! Return list of pointers to the Series objects of a given session
+  QList<AATreeNode_abstract*> getSeries(const AATreeNode_abstract* session);
+  //! Return list of pointers to the Shot objects of a given series
+  QList<AATreeNode_abstract*> getShots(const AATreeNode_abstract* series);
+  //! Add a session
+  AATreeNode_abstract* addSession(int index = -1);
+  //! Add a Series to a given session
+  AATreeNode_abstract* addSeries(const AATreeNode_abstract* session,
+                                 int index = -1);
+  //! Add a shot to a given series
+  AATreeNode_abstract* addShot(const AATreeNode_abstract* series,
+                               int index = -1);
+
+  bool removeSession(int index = -1);
+  bool removeSeries(const AATreeNode_abstract* session, int index = -1);
+
+  bool isModified() const;
+
  public slots:
   //! Convinience function to set the modified flag to true
   bool openConfigFile(const QString& fileName);
+
   //! Scan the config file to return the default paths where the images should
   //! be sought for
   QList<QDir> getImagesPaths();
+
+  //! Return the latest result (session, if empty, or series) that is older than
+  //! given date.
+  void getLastResult(QDateTime notYOungerThan = QDateTime());
+  //  void getSeriesByImagePath(const QString& imageFile);
 
  private:
   void setModified();
 
  private:
+  //! Root node, for more convenient navigation
   pugi::xml_node root;
+  //! Flag informing if the file has been opened
+  bool opened = false;
+  //! Flag informing, if the file has been modified
+  bool modified = false;
+  //! The actual XML data structure
+  pugi::xml_document pugi;
+  //! Pointer to the QFile object
+  QFile* m_file = nullptr;
+
  signals:
 };
 
