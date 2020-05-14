2a5f8a6583c1ac290499b93d8945791198864f05
grotsztaksel <grotsztaksel@o2.pl>
2020-05-14 23:42:55 +0200

    AATreeNode: Fix header

--- src/model/aatreenode_abstract.h
+++ src/model/aatreenode_abstract.h
@@ -21,6 +21,7 @@ class AATreeNode_abstract : public QObject {
   Q_OBJECT
  public:
   //! The constructor
+  //!
   explicit AATreeNode_abstract(QObject* parent = nullptr);
 
   /*------------------------------*
@@ -30,6 +31,7 @@ class AATreeNode_abstract : public QObject {
    *------------------------------*/
 
   //! Get type (name) of the element
+  //!
   virtual QString name() = 0;
 
   //! Identify the index at which this row resides in its parent node
@@ -47,9 +49,11 @@ class AATreeNode_abstract : public QObject {
   virtual bool writeToFile(const QFile& file) = 0;
 
   //! Get the root node
+  //!
   virtual AATreeNode_abstract* root();
 
   //! Get the node's parent node
+  //!
   virtual AATreeNode_abstract* parent() = 0;
 
   /*------------------------------*
@@ -59,21 +63,26 @@ class AATreeNode_abstract : public QObject {
    *------------------------------*/
 
   //! Get list of all attributes
+  //!
   virtual QStringList attributes() = 0;
 
   //! Get attribute of given type
+  //!
   virtual QVariant attribute(const QString name) = 0;
 
   //! check if the node has a given attribute
+  //!
   virtual bool hasAttribute(const QString name) = 0;
 
   //! set attribute of given name
+  //!
   virtual bool setAttribute(const QString name, const int& value) = 0;
   virtual bool setAttribute(const QString name, const QString& value) = 0;
   virtual bool setAttribute(const QString name, const double& value) = 0;
   virtual bool setAttribute(const QString name, const bool& value) = 0;
 
   //! clear attribute of given name
+  //!
   virtual bool removeAttribute(const QString name) = 0;
 
   /*------------------------------*
@@ -83,12 +92,20 @@ class AATreeNode_abstract : public QObject {
    *------------------------------*/
 
   //! Get list of direct children
+  //!
   virtual QVector<AATreeNode_abstract*> children() const = 0;
 
   //! Get list of children of given name
+  //!
   virtual QVector<AATreeNode_abstract*> children(const QString& name) const = 0;
+
   //! Get the child node at given index (starting from zero)
+  //!
   virtual AATreeNode_abstract* getChild(const int index) const = 0;
+
+  //! Get the index-th named child node (starting from zero)
+  //!
+  virtual AATreeNode_abstract* getChild(QString name, int index = 0) const = 0;
 
   //! Create child of given name then insert it before index-th node. If no
   //! index provided, or if the index exceeds the number of children, insert at
@@ -102,15 +119,14 @@ class AATreeNode_abstract : public QObject {
   virtual AATreeNode_abstract* insertChild(AATreeNode_abstract* child,
                                            int index = -1) = 0;
 
-  //  //! Remove index-th child of given name. If no index is provided, remove
+  //! Remove index-th child of given name. If no index is provided, remove
   //  last
-  //  //! child of this name
-  //  virtual AATreeNode_abstract* removeChild(const QString& name,
-  //                                           const int index = -1) = 0;
+  //! child of this name
+  virtual bool removeChild(const QString& name, const int index = -1) = 0;
 
   //  //! Remove index-th child, regardless of its name. If index not provided,
   //  //! remove last child
-  virtual AATreeNode_abstract* removeChild(const int index = -1) = 0;
+  virtual bool removeChild(const int index = -1) = 0;
   //
 };
 
