b9d44437764207eff289a8a89180a975513d520e
grotsztaksel <grotsztaksel@o2.pl>
2020-05-19 23:42:53 +0200

    AADirScanner: Implement first version of image scanning routine

--- src/application/aadirscanner.cpp
+++ src/application/aadirscanner.cpp
@@ -1,60 +1,96 @@
 #include "aadirscanner.h"
 #include <QDebug>
+#include <QDir>
+#include "exifreader.h"
+
+#define ATTRIBUTE_DIR "dir"
+#define NODE_PATH "path"
+#define PARENTNODE_PATH "imagePaths"
 
 AADirScanner::AADirScanner(AAObject* parent, const QStringList args)
-    : AAObject(parent, args) {}
+    : AAObject(parent, args) {
+  updateConfigFile();
+}
 
 void AADirScanner::setModel(AATreeModel* model) {
   m_model = model;
+  m_imgNode = m_model->root()->getChild(PARENTNODE_PATH);
+  getPaths();
 }
 
-void AADirScanner::setPath(const QString& path) {
+void AADirScanner::setPath(const QString& path,
+                           AATreeNode_abstract* modelNode) {
+  AATreeNode_abstract* imgNode = m_model->root()->getChild(PARENTNODE_PATH);
   if (!m_paths.contains(path)) {
     m_paths.append(path);
-  }
-}
-
-/*
-void AADirScanner::updateSessions() {
-  auto imageDir = data.getImagesPaths();
-  QFileInfoList fileInfos;
-  // get the list of inage files, sorted by creation time
-  for (QDir imageDir : data.getImagesPaths()) {
-    qDebug() << "Dir found: " << imageDir.absolutePath();
-    //    fileInfos << imageDir.entryInfoList(
-    //        settings->value("imagesFilter").toStringList(), QDir::Files,
-    //        QDir::Time | QDir::Reversed);
-  }
-  for (auto finfo : fileInfos) {
-    qDebug() << finfo.filePath()
-             << finfo.fileTime(QFileDevice::FileMetadataChangeTime);
-  }
-
-  QDateTime lastResult;  // = data.getLastResult();
-  qDebug() << lastResult;
-
-  if (lastResult.isValid()) {
-    for (auto finfo : fileInfos) {
-      if (finfo.fileTime(QFileDevice::FileMetadataChangeTime) < lastResult) {
-        fileInfos.removeOne(finfo);
-      } else {
-        break;  // Assuming the files were sorted by their creation date, there
-                // is no need to scan the remaining files
-      }
+    if (!imgNode->children().contains(modelNode)) {
+      auto newPath = imgNode->addChild(NODE_PATH);
+      newPath->setAttribute(ATTRIBUTE_DIR, path);
     }
   }
-
-  //  qint64 sessionInterval = getSessionInterval();
-  //  qint64 seriesInterval = getSessionInterval();
-
-  // go to the last save session/series to get the date
 }
-//    xml_node lastSeries =
-//
-cfg.pugi.document_element().child("sessions").last_child("session").last_child("series");
-//    if (lastSeries.empty()){
-//        //create the
+
+void AADirScanner::updateConfigFile() {
+  QString cfgFile = getSetting(CFG_FILE).toString();
+  m_cfgPath = QFileInfo(cfgFile).path();
+}
+
+void AADirScanner::getImagesFiles() {
+  QFileInfoList fileInfos;
+  QMap<QDateTime, QString> output;
+  ExifReader er;
+  for (QString path : m_paths) {
+    QDir dir(path);
+    fileInfos << dir.entryInfoList(getSetting(IMAGE_FILTER).toStringList(),
+                                   QDir::Files, QDir::Time | QDir::Reversed);
+  }
+  for (QFileInfo finfo : fileInfos) {
+    QFile file;
+    file.setFileName(finfo.absoluteFilePath());
+    bool ok = er.openFile(file);
+    qDebug() << finfo.fileName() << ok << er.imageDateTime()
+             << er.originalDateTime() << er.digitizedDateTime()
+             << finfo.fileTime(QFileDevice::FileMetadataChangeTime);
+  }
+}
+
+void AADirScanner::getPaths() {
+  for (auto child : m_imgNode->children(NODE_PATH)) {
+    QDir imgDir(m_cfgPath + "/" + child->attribute(ATTRIBUTE_DIR).toString());
+    if (imgDir.exists()) {
+      setPath(imgDir.absolutePath(), child);
+    }
+  }
+  getImagesFiles();
+}
+
+// void AADirScanner::updateSessions() {
+//  auto imageDir = data.getImagesPaths();
+//  QFileInfoList fileInfos;
+//  // get the list of inage files, sorted by creation time
+//  for (QDir imageDir : data.getImagesPaths()) {
+//    qDebug() << "Dir found: " << imageDir.absolutePath();
+//    fileInfos << imageDir.entryInfoList(
+//        settings->value("imagesFilter").toStringList(), QDir::Files,
+//        QDir::Time | QDir::Reversed);
+//  }
+//  for (auto finfo : fileInfos) {
+//    qDebug() << finfo.filePath()
+//             << finfo.fileTime(QFileDevice::FileMetadataChangeTime);
+//  }
+
+//  QDateTime lastResult;  // = data.getLastResult();
+//  qDebug() << lastResult;
+
+//  if (lastResult.isValid()) {
+//    for (auto finfo : fileInfos) {
+//      if (finfo.fileTime(QFileDevice::FileMetadataChangeTime) < lastResult) {
+//        fileInfos.removeOne(finfo);
+//      } else {
+//        break;  // Assuming the files were sorted by their creation date,
+//        there
+//                // is no need to scan the remaining files
+//      }
 //    }
-//    for (QFileInfo fileInfo : fileInfos){
-//    }
-*/
+//  }
+//}
--- src/application/aadirscanner.h
+++ src/application/aadirscanner.h
@@ -19,13 +19,44 @@ class AADirScanner : public AAObject {
   void setModel(AATreeModel* model);
 
  public slots:
-  //! Set a path to look for images. if the path already available, ignore.
+
+  //! Set a path to look for images. if the path already available, ignore. If
+  //! the path does not yet exist in the list, add it to the config file as well
   //!
-  void setPath(const QString& path);
+  void setPath(const QString& path, AATreeNode_abstract* modelNode = nullptr);
+
+  //! Call this function if the config file has been changed.
+  //!
+  void updateConfigFile();
+
+  //! Scan the perviously set paths and find image files. Return them sorted by
+  //! creation date/time taken either form EXIF metadata (if available), or by
+  //! creation date/time taken from system
+  //!
+  void getImagesFiles();
 
  private:
+  //! From the config file, read the <imagePath> nodes and, assuming that they
+  //! define paths relative to the config file, find the paths
+  //!
+  void getPaths();
+
+ private:
+  //! Config model
+  //!
   AATreeModel* m_model;
+
+  //!  List of alrady known paths
+  //!
   QStringList m_paths;
+
+  //! Path tho the config file (without the filename itself)
+  //!
+  QString m_cfgPath;
+
+  //! model node accessed by this class
+  //!
+  AATreeNode_abstract* m_imgNode = nullptr;
 };
 
 #endif  // AADIRSCANNER_H
