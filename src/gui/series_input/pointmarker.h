439658d44e1d2958c0cc5b9d4be4a04af7ddbe80
grotsztaksel <grotsztaksel@o2.pl>
2020-07-08 22:17:38 +0200

    PointMarker: Make event methods protected, as in parent class

--- src/gui/series_input/pointmarker.h
+++ src/gui/series_input/pointmarker.h
@@ -11,16 +11,17 @@ class PointMarker : public QGraphicsItem {
   PointMarker(QGraphicsItem* parent = nullptr);
   QRectF boundingRect() const override;
   int type() const override;
-  void mousePressEvent(QGraphicsSceneMouseEvent* event) override;
-  void mouseReleaseEvent(QGraphicsSceneMouseEvent* event) override;
-  void mouseMoveEvent(QGraphicsSceneMouseEvent* event) override;
-  void hoverEnterEvent(QGraphicsSceneHoverEvent* event) override;
-  void hoverLeaveEvent(QGraphicsSceneHoverEvent* event) override;
 
  protected:
   void paint(QPainter* painter,
              const QStyleOptionGraphicsItem* option,
              QWidget* widget = nullptr) override;
+  void mousePressEvent(QGraphicsSceneMouseEvent* event) override;
+  void mouseReleaseEvent(QGraphicsSceneMouseEvent* event) override;
+  void mouseMoveEvent(QGraphicsSceneMouseEvent* event) override;
+  void hoverEnterEvent(QGraphicsSceneHoverEvent* event) override;
+  void hoverLeaveEvent(QGraphicsSceneHoverEvent* event) override;
+  void keyPressEvent(QKeyEvent* event) override;
 
  protected:
   bool hovered = false;
