c3665b2286b7f2101de8e03edb4f0369a95d5cfe
grotsztaksel <grotsztaksel@o2.pl>
2020-07-26 01:08:07 +0200

    graphicsitems.h: Change enum item name to avoid name collision

--- src/gui/series_input/graphicsitems.h
+++ src/gui/series_input/graphicsitems.h
@@ -8,7 +8,7 @@ enum {
   Image = QGraphicsItem::UserType + 1,
   Point = QGraphicsItem::UserType + 2,
   Arrow = QGraphicsItem::UserType + 3,
-  TargetAreaCorner = QGraphicsItem::UserType + 4,
+  TargetAreaCornerPoint = QGraphicsItem::UserType + 4,
   TargetAreaEdgePoint = QGraphicsItem::UserType + 5,
   TargetAreaEdge = QGraphicsItem::UserType + 6
 };
